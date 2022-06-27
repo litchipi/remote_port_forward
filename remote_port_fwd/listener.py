ce3eeabfb18b0226c72aaac89adc668c8f872b93
Litchi Pi <litchi.pi@proton.me>
2022-06-27 09:40:36 +0200

    listener done, todo forwarder
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- remote_port_fwd/listener.py
+++ remote_port_fwd/listener.py
@@ -1,88 +1,125 @@
 #!/usr/bin/env python3
 
+# Taken from https://github.com/git-davi/reverse-python-port-forwarder
+
 import socket
-
-# TODO  Testing
-
-"""
-Open two listeners:
-    One get the connection from remote tunnel
-    Other get connection from Internet browser
-
-    Simple passthrough
-"""
+import time
+import sys
+import threading
+import argparse
 
 class Listener:
-    def __init__(self, portA, portB):
-        self.portA = portA
-        self.portB = portB
+    BUFFER_SIZE = 0x400
 
-        self.serverA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serverB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            self.serverA.bind(("localhost", portA))
-            self.serverB.bind(("localhost", portB))
-        except Exception as e:
-            print(f"Error while creating listener: {e}")
+    def __init__(self, tunnel_address, rppf_address):
+        try :
+            print("Waiting for incoming tunnel connection...")
+            tunnel_socket = self.make_listening_server(tunnel_address)
+            self.tunnel_conn, address  = tunnel_socket.accept()
+            print("Tunnel created")
+
+            print("Opening rppf listening service...")
+            self.rppf_socket = self.make_listening_server(rppf_address)
+            print("--------------------------------------------")
+            print("RPPF Service opened on " + str(rppf_address))
+            print("Ready to transfer data")
+
+            # start thread for incoming and outcoming traffic
+            tunnel2rppf_t = threading.Thread(target=self.tunnel2rppf)
+            rppf2tunnel_t = threading.Thread(target=self.rppf2tunnel)
+            tunnel2rppf_t.daemon = True
+            rppf2tunnel_t.daemon = True
+
+            # rppf lock (needed when connection get restarted)
+            self.rppf_conn_lock = threading.Lock()
+
+            # start threads only after connection is initiated with rppf
+            self.rppf_conn, address = self.rppf_socket.accept()
+
+            tunnel2rppf_t.start()
+            rppf2tunnel_t.start()
+            
+            tunnel2rppf_t.join()
+            rppf2tunnel_t.join()
+
+        except KeyboardInterrupt :
+            # close sockets after SIGINT
+            print('\nClosing connections')
+            tunnel_socket.shutdown(socket.SHUT_RDWR)
+            tunnel_socket.close()
+            self.rppf_socket.shutdown(socket.SHUT_RDWR)
+            self.rppf_socket.close()
+            print('\nStopped correctly')
+            sys.exit(0)
+
+        except Exception as e :
+            print('An exception occurred')
+            print(e)
+            print('Trying to close sockets')
+            tunnel_socket.shutdown(socket.SHUT_RDWR)
+            tunnel_socket.close()
+            self.rppf_socket.shutdown(socket.SHUT_RDWR)
+            self.rppf_socket.close()
             sys.exit(1)
 
-    def start(self):
-        self.serverA_thread = threading.Thread(
-            target = self.fwd,
-            args=(self.serverA, self.portB)
-        )
-        self.serverB_thread = threading.Thread(
-            target = self.fwd,
-            args=(self.serverB, self.portA)
-        )
-        self.serverA_thread.start()
-        self.serverB_thread.start()
 
-    def fwd(self, srv, to_port):
-        srv.listen(5)
-        while True:
-            client_socket, addr = srv.accept()
+    def make_listening_server(self, address) :
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(address)
+        server_socket.listen()
 
-            client_thread = threading.Thread(
-                target=self.conn_handler,
-                args=(client_socket, to_port)
-            )
-            client_thread.start()
+        return server_socket
 
-    def conn_handler(self, client_socket, to_port):
-        fwd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        fwd_socket.connect(("localhost", to_port))
-        while True:
-            recv_data = self.receive_data(client_socket)
-            if len(recv_data):
-                fwd_socket.send(recv_data)
+    # tunnel connection should be always alive and so should never launch error
+    # if tunnel connection drop you should restart the program
+    def tunnel2rppf(self):
+        while True :
+            data = self.tunnel_conn.recv(self.BUFFER_SIZE)
+            if not data :
+                raise Exception("Tunnel has dropped, this shouldn't happen, restart RPPF.")
+            
+            try :
+                self.rppf_conn_lock.acquire()
+                self.rppf_conn.sendall(data)
+                self.rppf_conn_lock.release()
+            except Exception :
+                raise Exception("RPPF connection has been closed")
 
-            fwd_data = self.receive_data(fwd_socket)
-            if len(fwd_data):
-                client_socket.send(fwd_data)
-
-            if not len(recv_data) and not len(fwd_data):
-                client_socket.close()
-                fwd_socket.close()
-                break
-
-    def receive_data(self, s, buffer_size=1024, timeout=2):
-        buffer = b""
-        s.settimeout(timeout)
-        try:
-            while True:
-                data = connection.recv(buffer_size)
-                if not data:
+    # rppf is listening and will accept incoming connections
+    def rppf2tunnel(self):
+        while True :
+            while True :
+                data = self.rppf_conn.recv(self.BUFFER_SIZE)
+                if not data :
                     break
-                buffer += data
-        except Exception as e:
-            pass
-        return buffer
+                try :
+                    self.tunnel_conn.sendall(data)
+                except Exception :
+                    raise Exception("Tunnel has dropped, this shouldn't happen, restart RPPF.")
 
-    def join(self):
-        self.serverA_thread.join()
-        self.serverB_thread.join()
+            self.rppf_conn_lock.acquire()
+            self.rppf_conn.shutdown(socket.SHUT_RDWR)
+            self.rppf_conn.close()
 
+            self.rppf_conn, address = rppf_socket.accept()
+            self.rppf_conn_lock.release()
+
+parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter, 
+        description="Raw TCP port forward")
+
+parser.add_argument("tunnel_address",
+        metavar="thost:tport",
+        type=str,
+        help="Tunnel address to listen to")
+
+parser.add_argument("serve_port",
+        type=int,
+        help="The port on which the tunnel serves the data")
 
 if __name__ == "__main__":
-    listener = Listener(8080, 8081)
+    args = parser.parse_args()
+
+    tunnel_hostname, tunnel_port = args.tunnel_address.split(':')
+    Listener((tunnel_hostname, int(tunnel_port)), ("localhost", args.serve_port))
